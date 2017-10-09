aa7d7c513a34be0651ee9b0318f47983b72d3d2d
Daniel Yang <danielyang112@gmail.com>
2017-10-10 00:09:38 +0100

    update tabbar icons

--- MedicConnect/Views/RadTabBarController/RadTabBar.h
+++ MedicConnect/Views/RadTabBarController/RadTabBar.h
@@ -12,10 +12,8 @@
 
 // Size
 #define TABBAR_HEIGHT 55.0f
-#define TABBAR_RECORD_BUTTON_DIAMETER 107.0f
-#define TABBAR_RECORD_BUTTON_PADDING -12.0f
 
 // Color
-#define COLOR_TABBAR_TINT [UIColor colorWithRed:60.0/255 green:60.0/255 blue:60.0/255 alpha:1.0]
+#define COLOR_TABBAR_TINT [UIColor colorWithRed:35/255 green:31/255 blue:32/255 alpha:1.0]
 
 @end
--- MedicConnect/Views/RadTabBarController/RadTabBarController.m
+++ MedicConnect/Views/RadTabBarController/RadTabBarController.m
@@ -21,15 +21,6 @@
     // Tab Selected Image Color
     [self.tabBar setTintColor:COLOR_TABBAR_TINT];
     
-    // Center Post Button
-    UIButton *centerButton = [[UIButton alloc] initWithFrame:CGRectMake((self.tabBar.frame.size.width - TABBAR_RECORD_BUTTON_DIAMETER)/2, TABBAR_RECORD_BUTTON_PADDING, TABBAR_RECORD_BUTTON_DIAMETER, TABBAR_RECORD_BUTTON_DIAMETER)];
-    [centerButton setBackgroundImage:[UIImage imageNamed:@"tab_icon_record"] forState:UIControlStateNormal];
-    [centerButton addTarget:self action:@selector(recordButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-    [self.tabBar addSubview:centerButton];
-    [self.tabBar setSelectionIndicatorImage:[[UIImage alloc] init]];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(gotoProfileScreen:) name:@"gotoProfileScreen" object:nil];
-    
 }
 
 -(void)gotoProfileScreen:(NSNotification *)notification {
@@ -38,10 +29,6 @@
     UINavigationController *nc = self.viewControllers[1];
     [nc dismissViewControllerAnimated:FALSE completion:nil];
     [nc popToRootViewControllerAnimated:false];
-}
-
-- (void)recordButtonPressed:(UIButton *)sender {
-    self.selectedIndex = 2;
 }
 
 - (void)didReceiveMemoryWarning {
