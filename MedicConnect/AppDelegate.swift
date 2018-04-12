2a633e093f95edb41859bac72ecdb98ea54f29c4
Daniel Yang <danielyang112@gmail.com>
2018-04-12 08:01:40 +0800

    remove firebase from the project

--- MedicConnect/AppDelegate.swift
+++ MedicConnect/AppDelegate.swift
@@ -37,6 +37,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Fabric
         Fabric.with([Crashlytics.self])
         
         let remoteNotif = launchOptions?[UIApplicationLaunchOptionsKey.remoteNotification] as? NSDictionary
